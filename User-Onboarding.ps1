efa93d6faed9ccd4b00f5eb20a08b58403658054
Elmerski <elmer.catapang@gmail.com>
2021-11-04 16:32:47 -0400

    add-files

---
+++ User-Onboarding.ps1
@@ -0,0 +1,12 @@
+﻿#Create a new AD user using a template
+
+$user_template = Get-ADUser -Identity _Template_IT-Coordin -Properties State,Department,Country,City,Description,MemberOf
+$user_template.UserPrincipalName = $null
+
+New-ADUser `
+    -Instance $user_template `
+    -Name 'Elmer Test' `
+    -SamAccountName 'etest' `
+    -UserPrincipalName 'etest@belusallc.com' `
+    -AccountPassword (Read-Host -AsSecureString "Password input") `
+    -Enabled $True
